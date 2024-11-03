85f9aadbb2dd9aeb3a4abad723960c19b0084958
KalminX <noriabure@gmail.com>
2024-11-03 08:37:42 +0100

    feat: solved the seventh task for promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(), uploadPhoto()]).then((values) => {
+    console.log(values);
+  });
+}
