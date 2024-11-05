d09aff9a397c54f358c72d0cf94fbd56812365b4
KalminX <noriabure@gmail.com>
2024-11-06 00:32:07 +0100

    update: fixed bugs in first task

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   return [
-    { id: 1, firstName: "Guillaume", string: "San Francisco" },
-    { id: 2, firstName: "James", string: "Columbia" },
-    { id: 3, firstName: "Serena", string: "San Francisco" },
+    { id: 1, firstName: 'Guillaume', string: 'San Francisco' },
+    { id: 2, firstName: 'James', string: 'Columbia' },
+    { id: 3, firstName: 'Serena', string: 'San Francisco' },
   ];
 }
