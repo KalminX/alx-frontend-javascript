ec86a66417dbb9b7e869239cdaf57dacfea7d297
KalminX <noriabure@gmail.com>
2024-11-06 04:49:49 +0100

    feat: fixed some bugs in proed=ct

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,9 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
+  if (typeof startString === 'function') {
+    return '';
+  }
   return Array.from(set)
     .filter((str) => str !== undefined && str.startsWith(startString))
     .map((str) => `${str.slice(startString.length)}`)
