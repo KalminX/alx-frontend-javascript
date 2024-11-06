0bb126daf1602bbf3235037cd541eba52629b585
KalminX <noriabure@gmail.com>
2024-11-06 11:28:49 +0100

    update: fixed bugs for the advanced task for data manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,7 @@
 export const weakMap = new WeakMap();
 export function queryAPI(api) {
   const count = weakMap.get(api) || 0;
-  if (count >= 5) {
+  if (count + 1 >= 5) {
     throw new Error('Endpoint load is high');
   }
   weakMap.set(api, count + 1);
