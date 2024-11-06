e0b408be9c9abc691a610574bb4ed031a6ff090a
KalminX <noriabure@gmail.com>
2024-11-06 03:23:22 +0100

    update: updated my if statement

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  if (position > length - 1 && position < 0) {
+  if (position > length - 1 || position < 0) {
     throw new Error('Position outside range');
   }
   const view = new DataView(buffer);
